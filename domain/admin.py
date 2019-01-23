e684c81ad28477131fb8c45520b5511c332ad685
chenhuazhong <571912102@qq.com>
2019-01-23 22:41:41 +0800

    my平台site 发布文章到csdn

--- domain/admin.py
+++ domain/admin.py
@@ -1,7 +1,34 @@
+import hashlib
+
 from django.contrib import admin
 
 # Register your models here.
+from utils.csdn_selenium import publish_csdn_page
 from . import models
 
-admin.site.register(models.Pager)
+
+class PagerAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        print('mark--page')
+        print(form.data['p_type'])
+        print(int(form.data['p_type']) == 1)
+        print('---',(int(form.data['p_type']) == 1) and (obj is None))
+        if int(form.data['p_type']) == 1:
+            if not change:
+                print(11)
+                # publish_csdn_page(form.data['p_title'], form.data['p_content'])
+                md = hashlib.md5(form.data['p_title'].encode('utf-8'))
+                title_md5 = md.hexdigest()
+                md2 = hashlib.md5(form.data['p_content'].encode('utf-8'))
+                content_md5 = md2.hexdigest()
+                # form.data['p_title_md5'] = title_md5
+                # form.data['p_content'] = content_md5
+            print(type(change))
+
+        # if obj is None :
+        else:
+            super().save_model(request, obj, form, change)
+
+
+admin.site.register(models.Pager, PagerAdmin)
 admin.site.register(models.Comment)
--- utils/csdn_selenium.py
+++ utils/csdn_selenium.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import time
 
+from selenium.webdriver.support.select import Select
+
+
 def spider_login_csdn():
 
     import os
@@ -23,8 +26,34 @@ def spider_login_csdn():
     print(driver.window_handles)
     print(driver.find_element_by_tag_name("button").text)
     driver.find_element_by_tag_name("button").click()
+    time.sleep(3)
+    driver.quit()
+    # driver.switch_to_frame()
+
+def publish_csdn_page(title, content):
+    # import os
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    # import django
+    # django.setup()
+    from users.models import User
+
+    url = "https://passport.csdn.net/login"
+    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+    driver.get(url)
+    driver.find_element_by_xpath('//div[@class="main-select"]//li[1]/a').click()
+    # 输入账号
+    csdn_username = User.objects.get(username='root').csdn_username
+    csdn_passworld = User.objects.get(username='root').csdn_passworld
+    driver.find_element_by_id('all').send_keys(csdn_username)
+    # 输入密码
+    driver.find_element_by_id('password-number').send_keys(csdn_passworld)
+    time.sleep(1)
+    print(driver.window_handles)
+    print(driver.find_element_by_tag_name("button").text)
+    driver.find_element_by_tag_name("button").click()
     print(driver.window_handles)
     time.sleep(8)
+
     # print(driver.find_element_by_tag_name("button").click())
     try:
         driver.find_element_by_xpath("//div[@class='indexSuperise']/button").click()
@@ -33,16 +62,30 @@ def spider_login_csdn():
     driver.find_element_by_xpath("//ul[@class='btns']/li[3]/a/span").click()
     time.sleep(3)
     try:
-        driver.find_element_by_link_text('开始创作').click()
+        print(driver.window_handles)
+        driver.switch_to_window(driver.window_handles[1])
+        driver.switch_to_alert()
+        driver.find_element_by_id('btnStart').click()
+        driver.switch_to_default_content()
     except Exception as e:
-        print('没有获取到开始') #todo
+        print('没有获取到开始', e)  # todo
         pass
-    driver.find_element_by_id('txtTitle').send_keys('haha')
+    driver.find_element_by_id('txtTitle').send_keys(title)
     driver.switch_to_frame(driver.find_element_by_xpath("//iframe[@class='cke_wysiwyg_frame cke_reset']"))
-    driver.find_element_by_xpath('//body/p').send_keys('haha')
-    time.sleep(3)
+    # driver.find_element_by_xpath('//body/p').click()
+    driver.find_element_by_xpath('//body').send_keys(content)
+    time.sleep(1)
+    driver.switch_to_default_content()
+    select1 = Select(driver.find_element_by_id('selType'))
+    select1.select_by_visible_text('原创')
+    select2 = Select(driver.find_element_by_id('radChl'))
+    select2.select_by_visible_text('后端')
+    time.sleep(1)
+    driver.find_element_by_id('btnPublish').click()
 
+    driver.quit()
+    # driver.switch_to_frame()
 if __name__ == '__main__':
-    spider_login_csdn()
+    publish_csdn_page('mysite publish','mysite publish')
 
 
